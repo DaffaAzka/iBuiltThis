73456e02cbfcd8f78426dbcaa9f3c91a8bc0f885
Azka <azkadaiki26@gmail.com>
2026-01-03 21:52:04 +0700

    build homepage (1)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h1>Hello NextJs</h1>
+      <span>Join thousands of creators sharing their work</span>
+      <h1>Share What You&apos;ve Built, Discover What&apos;s Launching</h1>
+      <p>
+        A community platform for creators to showcase their apps, AI tools, SaaS
+        products, and creative projects. Authentic launches, real builders,
+        genuine feedback.
+      </p>
+
+      <Button>Share your project</Button>
+      <Button>Explore project</Button>
     </>
   );
 }
